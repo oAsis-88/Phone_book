0af9a1a476f76cbebd9914c3a92a5e48285d842e
Ilya <ya.ir9@ya.ru>
2022-04-04 03:33:45 +0300

    app

---
+++ Phone_book/application.py
@@ -0,0 +1,30 @@
+# application.py
+
+from datetime import datetime
+import sys
+from PyQt5 import QtWidgets
+from authorization import Authorization
+from windowUser import MainWindowUser
+from mariaDB_users import MariaDBUsers
+
+
+def application():
+    app = QtWidgets.QApplication(sys.argv)
+
+    with open('cookie.txt', 'r') as cookie:
+        data = cookie.readline()
+        if data:
+            user = data.strip().split()[0]
+            if MariaDBUsers().get(user):
+                m = MainWindowUser(user)
+            else:
+                m = Authorization()
+        else:
+            m = Authorization()
+
+    m.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    application()
